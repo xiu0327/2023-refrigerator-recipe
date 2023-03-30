5a67a3db00950edfa63319e6f7c8a235b04fc9c5
j-kyung99 <apfwnls14@naver.com>
2023-03-30 10:37:35 +0900

    Design: 이미지 경로 변경

--- front/src/pages/login/Login.tsx
+++ front/src/pages/login/Login.tsx
@@ -6,7 +6,10 @@ import { Google, Github, EmojiSmile } from "react-bootstrap-icons";
 export default function Login() {
 	return (
 		<div className={styles.loginContainer}>
-			<img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/02627984-7272-438d-8692-9d1b52ced39e/%EB%83%89%EB%B6%80%ED%95%B41.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230327%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230327T061420Z&X-Amz-Expires=86400&X-Amz-Signature=3ae587a34dba7283049bfb5888818891bd39c31ff26ecc21b918c8fd9182e38f&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22%25EB%2583%2589%25EB%25B6%2580%25ED%2595%25B41.jpg%22&x-id=GetObject" />
+			<img
+				alt="냉장고를 부탁해"
+				src="https://velog.velcdn.com/images/j-kyung99/post/64842983-2c78-4362-ba99-365d867017bd/image.jpg"
+			/>
 			<div className={styles.loginLink}>
 				<span>
 					아직 회원이 아니신가요?
