9954d5028cf099e69079ecfabfbee734522b28ef
j-kyung99 <apfwnls14@naver.com>
2023-04-25 13:35:47 +0900

    Refactor: 로그인 화면 속 링크 컴포넌트화 작업

---
+++ front/src/components/member/LoginLink/LoginLink.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import styles from "./LoginLink.module.scss";
+
+export default function LoginLink() {
+	const LoginLinks = [
+		{
+			title: "회원가입",
+			ment: "아직 회원이 아니신가요?",
+			link: "../register/register",
+		},
+		{
+			title: "비밀번호 찾기",
+			ment: "비밀번호를 잊으셨나요?",
+			link: "../password/password",
+		},
+	];
+	return (
+		<div className={styles.loginLink}>
+			{LoginLinks.map((items) => (
+				<span key={items.title}>
+					{items.ment}
+					<Link legacyBehavior href={items.link}>
+						<a>{items.title}</a>
+					</Link>
+				</span>
+			))}
+		</div>
+	);
+}
