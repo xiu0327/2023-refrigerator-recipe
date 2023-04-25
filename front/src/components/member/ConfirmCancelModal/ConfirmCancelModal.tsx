9e02bfc560c7fc2ff8bdda6ce7b4c1b1780e5621
j-kyung99 <apfwnls14@naver.com>
2023-04-25 13:28:00 +0900

    Refactor: 확인/취소 모달 컴포넌트화 작업

---
+++ front/src/components/member/ConfirmCancelModal/ConfirmCancelModal.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import styles from "./ConfirmCancelModal.module.scss";
+
+export default function ConfirmCancelModal(props: {
+	title: string;
+	ment: string;
+}) {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<>
+			<Button className={styles.button} variant="primary" onClick={handleShow}>
+				{props.title}
+			</Button>
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header>
+					<Modal.Title>{props.title}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>{props.ment}</Modal.Body>
+				<Modal.Footer>
+					<Link legacyBehavior href="/">
+						<Button>확인</Button>
+					</Link>
+					<Button variant="secondary" onClick={handleClose}>
+						취소
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+}
