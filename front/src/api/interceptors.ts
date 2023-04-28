aa55352096635560b312388bc827d0031ea50324
mxxseonkim <mxxseonkim@gmail.com>
2023-04-29 06:25:53 +0900

    Feat: accessToken 토큰 재발행 자동화

---
+++ front/src/api/interceptors.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const instance = axios.create();
+
+instance.interceptors.response.use(
+	function (response) {
+		return response;
+	},
+	async function (error) {
+		const { response: errorResponse } = error;
+		const originalRequest = error.config;
+
+		if (errorResponse?.data?.error === "INVALID_ACCESS_TOKEN") {
+			// TODO: accessToken 재발행 후 실패한 요청(originalRequest) 재시도
+			return reissueAccessToken();
+		}
+
+		return Promise.reject(error);
+	},
+);
+
+const reissueAccessToken = () => {
+	const accessToken = localStorage.getItem("accessToken");
+	const refreshToken = localStorage.getItem("refreshToken");
+	axios
+		.post("/api/auth/reissue", {
+			accessToken: accessToken,
+			refreshToken: refreshToken,
+		})
+		.then(function (response) {
+			const accessToken = response.data.accessToken;
+			instance.defaults.headers.common[
+				"Authorization"
+			] = `Bearer ${accessToken}`;
+			localStorage.setItem("accessToken", accessToken);
+			console.log("토큰 재발행!");
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+};
+
+export default instance;
