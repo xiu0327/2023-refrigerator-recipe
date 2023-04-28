0b9dbc46bc9f2d7434d1f638b9a9cae6bf0e1377
mxxseonkim <mxxseonkim@gmail.com>
2023-04-29 06:26:20 +0900

    Feat: 로그인 API

---
+++ front/src/api/login.ts
@@ -0,0 +1,22 @@
+import instance from "./interceptors";
+
+export const login = (email: string, password: string) => {
+	instance
+		.post("/api/auth/login", {
+			email: email,
+			password: password,
+		})
+		.then(function (response) {
+			const { _, accessToken, refreshToken } = response.data;
+			instance.defaults.headers.common[
+				"Authorization"
+			] = `Bearer ${accessToken}`;
+			localStorage.setItem("accessToken", accessToken);
+			localStorage.setItem("refreshToken", refreshToken);
+			console.log("토큰 초기 설정 완료!");
+			console.log(accessToken);
+		})
+		.catch(function (error) {
+			console.log(error.response.data.error);
+		});
+};
