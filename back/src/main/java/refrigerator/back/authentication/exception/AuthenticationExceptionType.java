b71e10cddba53eda8181cc0c455d187ee56efa3d
xiu0327 <78461009+xiu0327@users.noreply.github.com>
2023-05-16 23:02:30 +0900

    Feat: 로그아웃 성공시 리다이렉트 경로 추가 (#59)
    * Feat: 최초 로그인 확인을 위한 init-member-info 헤더 추가
    
    * Feat: 로그아웃 성공시 리다이렉트 경로 추가

--- back/src/main/java/refrigerator/back/authentication/exception/AuthenticationExceptionType.java
+++ back/src/main/java/refrigerator/back/authentication/exception/AuthenticationExceptionType.java
@@ -13,7 +13,8 @@ public enum AuthenticationExceptionType implements BasicExceptionType {
     ALREADY_LOGIN_MEMBER("WRONG_REQUEST", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
     ALREADY_LOGOUT_MEMBER("ALREADY_LOGOUT_MEMBER", "이미 로그아웃된 사용자 입니다.", HttpStatus.BAD_REQUEST),
     NOT_INIT_PROFILE_AND_NICKNAME("NOT_INIT_PROFILE_AND_NICKNAME", "프로필과 닉네임이 초기화되지 않았습니다.", HttpStatus.BAD_REQUEST),
-    NOT_FOUND_TOKEN("NOT_FOUND_TOKEN", "토큰을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST)
+    NOT_FOUND_TOKEN("NOT_FOUND_TOKEN", "토큰을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
+    FAIL_REDIRECT("FAIL_REDIRECT", "리다이렉트 경로가 잘못되었습니다.", HttpStatus.BAD_REQUEST)
     ;
 
     private final String errorCode;
