5c8ae1a81b35e94f3e633e4bbc4d645234e8bec4
j-kyung99 <apfwnls14@naver.com>
2023-05-20 16:28:59 +0900

    Refactor: 회원 탈퇴 디렉토리 재구성 및 화면 구현

--- front/src/pages/member/unregister/index.tsx
+++ front/src/pages/member/unregister/index.tsx
@@ -3,17 +3,28 @@ import styles from "./unregister.module.scss";
 import { Button } from "react-bootstrap";
 import { useState } from "react";
 import UnregisterModal from "@/components/member/UnregisterModal/UnregisterModal";
+import { login } from "@/api/login";
 
 export default function Unregister() {
+	const [password, setPassword] = useState();
 	const [showModal, setShowModal] = useState(false);
 
 	const handleClose = () => setShowModal(false);
 	const handleShow = () => setShowModal(true);
+	//login("gmail@gmail.com", "gmail123!!");
+
+	const onPasswordHandler = (e: any) => {
+		setPassword(e.target.value);
+	};
 
 	return (
 		<div className={styles.loginEmailContainer}>
 			<span className={styles.loginEmailTitle}>회원탈퇴</span>
-			<InputContent title="비밀번호" />
+			<InputContent
+				title="비밀번호"
+				type="password"
+				onChange={onPasswordHandler}
+			/>
 			<Button
 				className={styles.linkButton}
 				variant="primary"
@@ -22,7 +33,11 @@ export default function Unregister() {
 				다음으로
 			</Button>
 			{/* 비밀번호가 맞으면 모달 띄우기 */}
-			<UnregisterModal show={showModal} handleClose={handleClose} />
+			<UnregisterModal
+				show={showModal}
+				handleClose={handleClose}
+				password={password}
+			/>
 		</div>
 	);
 }
