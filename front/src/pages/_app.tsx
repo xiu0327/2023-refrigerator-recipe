9c1e65f581d46dbff221ab07172ab15f2628457a
j-kyung99 <apfwnls14@naver.com>
2023-05-24 14:52:55 +0900

    Chore: 타이틀 수정

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>냉장고를 부탁해</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
