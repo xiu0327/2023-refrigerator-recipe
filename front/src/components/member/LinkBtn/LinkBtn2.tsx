76d4d9fd94823e563a04ff2abc55e0e1e1c9f87b
j-kyung99 <apfwnls14@naver.com>
2023-05-24 15:26:57 +0900

    Feat: 링크 태그 삭제

--- front/src/components/member/LinkBtn/LinkBtn2.tsx
+++ front/src/components/member/LinkBtn/LinkBtn2.tsx
@@ -3,19 +3,17 @@ import { Button } from "react-bootstrap";
 import styles from "./LinkBtn.module.scss";
 import { login } from "@/api/login";
 
-export default function LinkBtn2({ title, link, email, password }) {
+export default function LinkBtn2({ title, email, password }) {
 	return (
-		<Link legacyBehavior href={link}>
-			<Button
-				className={styles.linkButton}
-				variant="primary"
-				size="lg"
-				onClick={() => {
-					login(email, password);
-				}}
-			>
-				<span>{title}</span>
-			</Button>
-		</Link>
+		<Button
+			className={styles.linkButton}
+			variant="primary"
+			size="lg"
+			onClick={() => {
+				login(email, password);
+			}}
+		>
+			<span>{title}</span>
+		</Button>
 	);
 }
