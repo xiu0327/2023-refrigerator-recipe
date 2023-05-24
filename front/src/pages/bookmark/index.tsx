c78fb4b89143d0c2808c39b5f486bff2f2238a10
j-kyung99 <apfwnls14@naver.com>
2023-05-24 15:37:02 +0900

    Chore: 병합

---
+++ front/src/pages/bookmark/index.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import router from "next/router";
+import { getRecipeList } from "@/api";
+import { RecipePreview } from "@/types/types";
+
+import AppNavLayout from "@/components/layout/AppNavLayout";
+import SearchBar from "@/components/global/SearchBar/SearchBar";
+import FilterBar from "@/components/recipe/FilterBar/FilterBar";
+import RecipeList from "@/components/recipe/RecipeList/RecipeList";
+
+import styles from "@/scss/pages.module.scss";
+import { login } from "@/api/login";
+import RecipeGrid from "@/components/recipe/RecipeGrid/RecipeGrid";
+
+export default function RecipeListPage() {
+	// TEMP: 로그인 화면과 연결하면 삭제
+	// useEffect(() => {
+	// 	login();
+	// }, []);
+
+	const [page, setPage] = useState(0);
+	const [recipeData, setRecipeData] = useState<RecipePreview[]>([]);
+
+	const [show, setShow] = useState(false);
+	const [filterMenuList, setFilterMenuList] = useState([]);
+
+	useEffect(() => {
+		const fetchRecipeData = async () => {
+			const data = await getRecipeList(page);
+			setRecipeData((prev) => [...prev, ...data]);
+		};
+		fetchRecipeData();
+	}, [page]);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				const firstEntry = entries[0];
+				firstEntry.isIntersecting && setPage((prev) => prev + 1);
+			},
+			{ threshold: 1 },
+		);
+		observer.observe(document.querySelector("#end-of-list"));
+		return () => observer.disconnect();
+	}, []);
+
+	return (
+		<AppNavLayout title="북마크">
+			<div style={{ padding: "1rem" }}>
+				<RecipeGrid recipeData={recipeData} />
+			</div>
+			<div id="end-of-list"></div>
+		</AppNavLayout>
+	);
+}
