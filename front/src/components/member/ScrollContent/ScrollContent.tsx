c2b6691c7fb0dbff63def46b1aa843e82986b5c9
j-kyung99 <apfwnls14@naver.com>
2023-05-24 15:27:45 +0900

    Feat: 더보기 기능 추가

--- front/src/components/member/ScrollContent/ScrollContent.tsx
+++ front/src/components/member/ScrollContent/ScrollContent.tsx
@@ -1,30 +1,62 @@
-import instance from "@/api/interceptors";
+import router from "next/router";
 import styles from "./ScrollContent.module.scss";
-import { useEffect, useState } from "react";
 
 export default function ScrollContent(props: any) {
 	const onClickHandler = () => {};
+	const onClickPlusHandler = (content: string) => {
+		router.push(`/${content}`);
+	};
 	return props.content === "star" ? (
 		<div className={styles.scroll}>
-			{props.scorePreview.map((i) => (
+			{props.starPreview?.map((i) => (
 				<div className={styles.content} key={i.scoreId}>
 					<button className={styles.recipeBtn} onClick={onClickHandler}>
-						<img className={styles.recipeImage} src={i.image} />
+						<img className={styles.recipeImage} src={i.recipeImage} />
 					</button>
-					<div className={styles.recipeName}>{i.scoreName}</div>
+					<div className={styles.recipeNameWrapper}>
+						<div className={styles.recipeName}>{i.recipeName}</div>
+					</div>
 				</div>
 			))}
+			<div className={styles.content}>
+				<button
+					className={styles.recipeBtn__plus}
+					onClick={() => {
+						onClickPlusHandler("star");
+					}}
+				>
+					<span>
+						<span className={styles.plus}>+</span>
+						<span>더보기</span>
+					</span>
+				</button>
+			</div>
 		</div>
 	) : (
 		<div className={styles.scroll}>
-			{props.bookmarkPreview.map((i) => (
+			{props.bookPreview?.map((i) => (
 				<div className={styles.content} key={i.recipeId}>
 					<button className={styles.recipeBtn} onClick={onClickHandler}>
-						<img className={styles.recipeImage} src={i.image} />
+						<img className={styles.recipeImage} src={i.recipeImage} />
 					</button>
-					<div className={styles.recipeName}>{i.recipeName}</div>
+					<div className={styles.recipeNameWrapper}>
+						<div className={styles.recipeName}>{i.recipeName}</div>
+					</div>
 				</div>
 			))}
+			<div className={styles.content}>
+				<button
+					className={styles.recipeBtn__plus}
+					onClick={() => {
+						onClickPlusHandler("bookmark");
+					}}
+				>
+					<span>
+						<span className={styles.plus}>+</span>
+						<span>더보기</span>
+					</span>
+				</button>
+			</div>
 		</div>
 	);
 }
