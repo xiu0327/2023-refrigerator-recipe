d1b9e09a1e450a8059b3d8509df9982f1d2ec246
j-kyung99 <apfwnls14@naver.com>
2023-05-24 15:37:53 +0900

    Feat: 비밀번호 찾기 후 변경으로 이동 구현

--- front/src/pages/member/password/find/index.tsx
+++ front/src/pages/member/password/find/index.tsx
@@ -1,19 +1,12 @@
-import styles from "./styles.module.scss";
 import { useState } from "react";
-import InputContent from "@/components/member/InputContent/InputContent";
-import ModalOnBtn2 from "@/components/member/ModalOnBtn/ModalOnBtn2";
-import InputBtn from "@/components/member/InputBtn/InputBtn";
-import { authenticate } from "@/api/athenticate";
-import { getCheckEmail } from "@/api/getCheckEmail";
+import router from "next/router";
 import { Button } from "react-bootstrap";
-import ChangePwModal from "@/components/member/ChangePwModal/ChangePwModal";
+import styles from "./styles.module.scss";
+import InputContent from "@/components/member/InputContent/InputContent";
+import { findPassword } from "@/api/password";
 
 export default function change() {
-	const [showModal, setShowModal] = useState(false);
 	const [email, setEmail] = useState("");
-
-	const handleClose = () => setShowModal(false);
-	const handleShow = () => setShowModal(true);
 
 	const onEmailHandler = (e: any) => {
 		setEmail(e.target.value);
@@ -23,13 +16,24 @@ export default function change() {
 		<div className={styles.passwordContainer}>
 			<span className={styles.passwordTitle}>비밀번호 찾기</span>
 			<InputContent title="이메일" type="email" onChange={onEmailHandler} />
+
 			<Button
 				className={styles.linkButton}
 				variant="primary"
-				onClick={handleShow}
+				onClick={() => {
+					findPassword(email)
+						.then(() => {
+							router.push("/member/password/change");
+						})
+						.catch((error) => {
+							alert(error.data.message);
+							location.reload();
+						});
+				}}
 			>
 				다음으로
 			</Button>
+
 			{/* 비밀번호 변경 화면으로 이동 */}
 		</div>
 	);
