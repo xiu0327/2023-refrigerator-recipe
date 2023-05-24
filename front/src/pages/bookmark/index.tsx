531032c9373a19cee280baec9d1853f4731b4b6a
mxxseonkim <mxxseonkim@gmail.com>
2023-05-24 17:14:45 +0900

    Fix: 북마크 목록 스크롤 구현 (Issue #86)

--- front/src/pages/bookmark/index.tsx
+++ front/src/pages/bookmark/index.tsx
@@ -1,38 +1,35 @@
 import { useEffect, useState } from "react";
 import { getBookmarks } from "@/api";
 
+import { useIntersectionObserver } from "@/hooks";
 import { Bookmark } from "@/types";
 
 import AppNavLayout from "@/components/layout/AppNavLayout";
 import RecipeGallery from "@/components/recipe/RecipeGallery/RecipeGallery";
 
 export default function BookmarkPage() {
-	const [page, setPage] = useState(0);
 	const [bookmarkData, setBookmarkData] = useState<Bookmark[]>([]);
+	const [page, setPage] = useState(0);
+	const [isDataLoaded, setIsDataLoaded] = useState(false);
+	const [isScrollEnd, setIsScrollEnd] = useState(false);
 
 	useEffect(() => {
-		(async () => {
-			const data = await getBookmarks(page);
-			setBookmarkData((prev) => [...prev, ...data]);
-		})();
+		!isScrollEnd &&
+			(async () => {
+				const data = await getBookmarks(page);
+				data.length !== 0
+					? setBookmarkData((prev) => [...prev, ...data])
+					: setIsScrollEnd(true);
+				setIsDataLoaded(true);
+			})();
 	}, [page]);
 
-	useEffect(() => {
-		const observer = new IntersectionObserver(
-			(entries) => {
-				const firstEntry = entries[0];
-				firstEntry.isIntersecting && setPage((prev) => prev + 1);
-			},
-			{ threshold: 1 },
-		);
-		observer.observe(document.querySelector("#end-of-list"));
-		return () => observer.disconnect();
-	}, []);
+	useIntersectionObserver(setPage, isDataLoaded);
 
 	return (
 		<AppNavLayout title="북마크">
 			<RecipeGallery recipeData={bookmarkData} />
-			<div id="end-of-list"></div>
+			{isDataLoaded && <div id="end-of-list"></div>}
 		</AppNavLayout>
 	);
 }
