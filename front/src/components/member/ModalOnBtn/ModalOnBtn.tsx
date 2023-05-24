9e4dbd188d15ed0410a206b2f38f5c62ce875ea2
j-kyung99 <apfwnls14@naver.com>
2023-05-24 15:28:54 +0900

    Refactor: register api 중복 사용 제거

--- front/src/components/member/ModalOnBtn/ModalOnBtn.tsx
+++ front/src/components/member/ModalOnBtn/ModalOnBtn.tsx
@@ -3,39 +3,10 @@ import { Button } from "react-bootstrap";
 import ConfirmModal from "../ConfirmModal/ConfirmModal";
 import styles from "./ModalOnBtn.module.scss";
 import axios from "axios";
+import { register } from "@/api/register";
 
 export default function ModalOnBtn({ title, ment, email, password, nickname }) {
 	const [showModal, setShowModal] = useState(false);
-
-	const register = (email: string, password: string, nickname: string) => {
-		let data = JSON.stringify({
-			email: email,
-			password: password,
-			nickname: nickname,
-		});
-
-		let config = {
-			method: "post",
-			maxBodyLength: Infinity,
-			url: "/api/members/join",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			data: data,
-		};
-
-		axios
-			.request(config)
-			.then((response) => {
-				setShowModal(true);
-			})
-			.catch((error) => {
-				if (error.response.data.code === "NOT_EMPTY_INPUT_DATA") {
-					setShowModal(false);
-					alert(error.response.data.message);
-				}
-			});
-	};
 
 	return (
 		<div className={`d-grid gap-2`}>
