b5b1e773fcf0aed36028858cf8194f5a40ea682d
j-kyung99 <apfwnls14@naver.com>
2023-05-25 13:36:20 +0900

    Feat: 비밀번호 변경 시 이메일 인증 불필요로 인한 코드 제거

--- front/src/pages/member/password/change/index.tsx
+++ front/src/pages/member/password/change/index.tsx
@@ -75,32 +75,6 @@ export default function change() {
 				</div>
 			</div>
 
-			<div>
-				<InputBtn
-					formTitle="이메일 인증"
-					btnTitle="인증번호 전송"
-					onChange={onCheckEmailHandler}
-					onClick={onCheckEmailClick}
-				/>
-				<div className={styles.clickBtn}>
-					{clickBtn ? (
-						<span className={styles.clickTrue}>
-							<span>
-								인증번호가 전송되었습니다. 30분 이내에 입력 부탁드리며,
-							</span>
-							<span>메일이 오지 않을 경우 스팸 메일함을 확인해주세요!</span>
-						</span>
-					) : (
-						<span className={styles.clickFalse}></span>
-					)}
-				</div>
-			</div>
-			<InputBtn
-				formTitle="인증번호 입력"
-				btnTitle="인증번호 확인"
-				onChange={onAuthenticateHandler}
-				onClick={onAuthenticateClick}
-			/>
 			<ModalOnBtn2 title="변경하기" ment="변경" password={newPassword} />
 		</div>
 	);
