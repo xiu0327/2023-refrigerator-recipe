c87021632ce1349cb3968f4561c3113654623977
j-kyung99 <apfwnls14@naver.com>
2023-05-25 15:24:29 +0900

    Feat: 엔터 입력 시 로그인 후 페이지 이동 구현

--- front/src/components/member/InputContent/InputContent.tsx
+++ front/src/components/member/InputContent/InputContent.tsx
@@ -2,6 +2,11 @@ import { Form, InputGroup } from "react-bootstrap";
 import styles from "./InputContent.module.scss";
 
 export default function InputContent(props: any) {
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter" && props.onEnterPress) {
+			props.onEnterPress();
+		}
+	};
 	return (
 		<InputGroup className={`${styles.loginEmailInput} mb-3`}>
 			<Form.Control
@@ -9,6 +14,7 @@ export default function InputContent(props: any) {
 				aria-label="Recipient's username"
 				aria-describedby="basic-addon2"
 				onChange={props.onChange}
+				onKeyDown={handleKeyDown}
 				type={props.type}
 			/>
 		</InputGroup>
--- front/src/pages/member/email/index.tsx
+++ front/src/pages/member/email/index.tsx
@@ -1,3 +1,4 @@
+import { login } from "@/api/login";
 import styles from "./styles.module.scss";
 import InputContent from "@/components/member/InputContent/InputContent";
 import LinkBtn2 from "@/components/member/LinkBtn/LinkBtn2";
@@ -6,26 +7,40 @@ import { SetStateAction, useState } from "react";
 export default function LoginEmail() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const onEmailHandler = (e: { target: { value: SetStateAction<string> } }) => {
+
+	const onEmailHandler = (e) => {
 		setEmail(e.target.value);
 	};
 
-	const onPasswordHandler = (e: {
-		target: { value: SetStateAction<string> };
-	}) => {
+	const onPasswordHandler = (e) => {
 		setPassword(e.target.value);
 	};
+
+	const handleLogin = () => {
+		login(email, password);
+	};
+
+	const handleEnterPress = () => {
+		handleLogin();
+	};
+
 	return (
 		<div className={styles.loginEmailContainer}>
 			<span className={styles.loginEmailTitle}>로그인</span>
-			<InputContent type="email" title="이메일" onChange={onEmailHandler} />
+			<InputContent
+				type="email"
+				title="이메일"
+				onChange={onEmailHandler}
+				onEnterPress={handleEnterPress}
+			/>
 			<InputContent
 				type="password"
 				title="비밀번호"
 				onChange={onPasswordHandler}
+				onEnterPress={handleEnterPress}
 			/>
+
 			<LinkBtn2 title={"로그인"} email={email} password={password} />
-			{/* 링크에 회원 냉장고로 이동하게 만들기 */}
 		</div>
 	);
 }
