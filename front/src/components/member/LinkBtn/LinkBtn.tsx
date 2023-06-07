5ff19190ffb1564b4882bf86e66223b1ae0c03d5
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-06-07 10:41:04 +0900

    Front jk v2 (#100)
    * Fix: 레시피 북마크 여부 오류 수정
    
    * Feat: 비밀번호 변경 오류 발생 시 새로고침 구현
    
    * Feat: 로그인 실패 시 새로고침 구현 제거
    
    * Chore: 로그아웃 alert 멘트 추가
    
    * Chore: 불필요한 import 제거
    
    * Chore: 닉네임 변경 멘트 수정
    
    * Feat: 마이페이지 화면 구현 수정
    
    * Feat: 닉네임 변경 페이지 구현
    
    * Style: 이메일 로그인 아이콘 변경
    
    * Feat: 링크버튼 컴포넌트에 닉네임 변경 추가
    
    * Style: 스타일 수정
    
    * Refactor: BackLayout 사용
    
    * Style: Watch Sass 사용
    
    ---------
    
    Co-authored-by: mxxseonkim <mxxseonkim@gmail.com>

--- front/src/components/member/LinkBtn/LinkBtn.tsx
+++ front/src/components/member/LinkBtn/LinkBtn.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Button } from "react-bootstrap";
 import styles from "./LinkBtn.module.scss";
+import { GearFill, PencilFill } from "react-bootstrap-icons";
 
 interface LinkBtnProps {
 	title: string;
@@ -10,8 +11,13 @@ interface LinkBtnProps {
 export default function LinkBtn({ title, link }: LinkBtnProps) {
 	return (
 		<Link legacyBehavior href={link}>
-			<Button className={styles.linkButton} variant="primary" size="lg">
-				<span>{title}</span>
+			<Button className={styles.linkButton} variant="light" size="lg">
+				{title === "비밀번호 변경" ? (
+					<GearFill className={styles.mypageIcon} />
+				) : (
+					<PencilFill className={styles.mypageIcon} />
+				)}
+				{title}
 			</Button>
 		</Link>
 	);
---
+++ front/src/pages/member/nickname/index.tsx
@@ -0,0 +1,34 @@
+import styles from "./styles.module.scss";
+import { useState } from "react";
+import InputContent from "@/components/member/InputContent/InputContent";
+import BackLayout from "@/components/layout/BackLayout";
+import ModalOnBtn3 from "@/components/member/ModalOnBtn/ModalOnBtn3";
+
+export default function nickname() {
+	const [nick, setNick] = useState("");
+
+	const onNicknameHandler = (e: any) => {
+		setNick(e.target.value);
+	};
+
+	return (
+		<BackLayout title={"닉네임 변경"}>
+			<div className={styles.passwordContainer}>
+				<div>
+					<InputContent
+						title="닉네임"
+						type="text"
+						onChange={onNicknameHandler}
+					/>
+					<div>
+						<span className={styles.regularPw}>
+							<span>영어, 한글, 띄어쓰기 포함 3자리 이상 10자리 이하 문자</span>
+						</span>
+					</div>
+				</div>
+
+				<ModalOnBtn3 title="변경하기" ment="변경" nick={nick} />
+			</div>
+		</BackLayout>
+	);
+}
