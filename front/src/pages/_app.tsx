24168f1922fb79939d6a353872fda4167982e0a5
mxxseonkim <mxxseonkim@gmail.com>
2023-06-14 21:21:02 +0900

    Feat: 리덕스 저장소 생성 및 적용

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -4,6 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import { silentRefresh } from "@/api/login";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 
 export default function App({ Component, pageProps }: AppProps) {
 	const [refreshTrigger, setRefreshTrigger] = useState(false);
@@ -16,11 +18,11 @@ export default function App({ Component, pageProps }: AppProps) {
 	}, []);
 
 	return (
-		<>
+		<Provider store={store}>
 			<Head>
 				<title>냉장고를 부탁해</title>
 			</Head>
 			{refreshTrigger && <Component {...pageProps} />}
-		</>
+		</Provider>
 	);
 }
